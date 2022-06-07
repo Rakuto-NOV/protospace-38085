9bcfe81e8cbaf156817f8863979a2609bf93849d
Rakuto-NOV <okudarakuto@gmail.com>
2022-06-07 10:38:37 +0900

    他のユーザーのプロトタイプ編集ページへ遷移出来てしまうので、トップページにリダイレクトするように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -25,9 +26,6 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
-    unless @prototype.user_id == current_user.id
-    end
   end
 
   def update
@@ -56,4 +54,7 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def move_to_index
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
